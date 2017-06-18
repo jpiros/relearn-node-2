e3e50483a22c372bbd322c44bf57f70664a17323
Jason Piros <jahsan87@gmail.com>
2017-06-17 18:31:20 -0700

    dealing with cors

--- server/server.js
+++ server/server.js
@@ -9,6 +9,19 @@ const { User } = require('./models/user');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
